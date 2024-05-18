d1be6e593dc0490ea7e2f59fe5a0cdef4c4e9409
treasurejeffy <treasurjeffy@gmail.com>
2024-05-18 22:59:09 +0100

    added commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,13 +10,13 @@ export default function Footer() {
     return(
         <section className="my-footer">
             <Container>
-                <Row  className="justify-content-center py-5">
-                    <Col className="col-lg-4 col-12 d-flex align-items-center justify-content-center ">
+                <Row  className="justify-content-center pb-5">
+                    <Col className="col-lg-3 col-12 d-flex col-footer-logo align-items-center justify-content-center ">
                         <img src={Logo} alt="Logo" className="Logos"/>
                     </Col>
-                    <Row className="col-lg-8 col-12" >
-                        <Col className="col-lg-4 col-md-6 col-12 mt-4">
-                        <h4 className="fs-5 text-center link-heading">Links</h4>
+                    <Row className="col-lg-9 col-12" >
+                        <Col className="col-lg-3 col-md-6 col-12 pt-5">
+                        <h4 className="fs-5 text-center fw-bold link-heading">Links</h4>
                             <div className="d-flex justify-content-center">
                                 <ul  className="ms-2 mt-3 link-list">
                                     <li>Home</li>
@@ -28,22 +28,21 @@ export default function Footer() {
                                 </ul>
                             </div>
                         </Col>
-                        <Col className="text-center  col-md-6  col-lg-4 col-12 mt-4">
-                            <h4 className="fs-5 social-media-heading">Social Media</h4>
+                        <Col className="text-center  col-md-6  col-lg-4 col-12 pt-5 mb-3">
+                            <h4 className="fs-5 fw-bold social-media-heading">Social Media</h4>
                             <div className="d-flex justify-content-center">
                                 <ul className="ms-5 mt-3 social-media-list">
-                                    <li className="d-flex"> <Instagram className="fs-4 me-2"/> Instagram</li>
-                                    <li className="d-flex "> <Facebook className="fs-4  me-2"/> Facebook</li>
-                                    <li className="d-flex"> <Linkedin className="fs-4  me-2"/> LinkedIn</li>
+                                    <li className="d-flex"> <Instagram className="fs-4 me-2 text-primary"/> Instagram</li>
+                                    <li className="d-flex "> <Facebook className="fs-4 text-primary me-2"/> Facebook </li>
+                                    <li className="d-flex"> <Linkedin className="fs-4 text-primary me-2"/> LinkedIn</li>
                                     <li className="d-flex"> <TwitterX className="fs-4  me-2"/> Twitter</li>
                                     <li className="d-flex"> <Tiktok className="fs-4  me-2"/> Tiktok</li>
-                                    <li className="d-flex"> <Whatsapp className="fs-4  me-2"/>090 1006 5284</li>
+                                    <li className="d-flex"> <Whatsapp className="fs-4 text-success me-2"/>090 1006 5284</li>
                                 </ul>
                             </div>
                         </Col>
-                        <Col className="col-lg-4 col-12 d-flex align-items-center justify-content-center">
-                            <img src={CEO} alt="CEO" className="footer-img mt-4"/>
-                            <span className="fs-4 img-span">CEO</span>
+                        <Col className="col-lg-5 col-12 d-flex align-items-center col-footer-img justify-content-center">
+                            <img src={CEO} alt="CEO" className="footer-img mt-4"/>                        
                         </Col>
                         <Col className="text-end fs-6 fw-bold text-danger pt-4 motto"> <p> Motto: Design Moderatly!</p></Col>
                     </Row>
